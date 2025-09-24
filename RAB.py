eb08550cd20f0fb44bb92730083c3b27b0b3e204
RNikdata <162301716+RNikdata@users.noreply.github.com>
2025-09-24 15:06:45 +0530

    Update RAB.py

--- RAB.py
+++ RAB.py
@@ -156,6 +156,18 @@ with tab1:
     # Display table
     columns_to_show = ["Manager Name","Account Name","Employee Id", "Employee Name", "Designation"]
     columns_to_show = [col for col in columns_to_show if col in filtered_df_unique.columns]
+    # Apply filters
+    if account_filter:
+        filtered_df = filtered_df[filtered_df["Account Name"].isin(account_filter)]
+    if manager_filter:
+        filtered_df = filtered_df[
+        (filtered_df["Manager Name"].isin(manager_filter))
+    ] 
+    if resource_search:
+        filtered_df = filtered_df[
+            filtered_df["Employee Name"].str.contains(resource_search, case=False, na=False) |
+            filtered_df["Employee Id"].astype(str).str.contains(resource_search, na=False)
+        ]
     st.dataframe(filtered_df_unique[columns_to_show], use_container_width=True, height=500, hide_index=True)
     
 # --- Tab 2: Swap Requests ---
