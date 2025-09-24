c652e9d52a6cef9d9c05c3d0b5fc47d28bb1c0f1
RNikdata <162301716+RNikdata@users.noreply.github.com>
2025-09-24 12:04:39 +0530

    Update RAB.py

--- RAB.py
+++ RAB.py
@@ -37,12 +37,7 @@ merged_df = df.merge(
 )
 
 # --- Sidebar: Logo & Company Name ---
-#company_logo_url = 
-company_name = "Mu Sigma"
-
 st.sidebar.image("logo.jpeg", width=150)
-st.sidebar.markdown(f"<h2 style='text-align:center'>{company_name}</h2>", unsafe_allow_html=True)
-st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
 # --- Sidebar Filters ---
 st.sidebar.markdown("<br><br>",unsafe_allow_html = True)
