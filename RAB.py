6d7354538da18af8213e3da1400adbc63f4c55f7
RNikdata <162301716+RNikdata@users.noreply.github.com>
2025-09-25 18:54:01 +0530

    Update RAB.py

--- RAB.py
+++ RAB.py
@@ -450,7 +450,7 @@ elif st.session_state["active_page"] == "Employee Transfer Form":
     with col2:
         interested_employee_add = st.selectbox(
             "Interested Employee",
-            options=["Select Interested Employee"] + available_employees["Employee Id"].dropna().tolist(),
+            options=["Select Interested Employee"] + (available_employees["Employee Id"] + "-" + available_employees["Employee Name"]).dropna().tolist(),
             key="interested_employee_add"
         )
     with col3:
